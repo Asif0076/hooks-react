d13596d99b6f1193cfd724d5aeee86e8a56a0f43
Asif Nawaz <assu.nawaz007@gmail.com>
2022-12-03 00:20:34 +0500

    added useMomo() function

--- src/App.js
+++ src/App.js
@@ -13,9 +13,10 @@ const updateCounterFromChild = useCallback(() => setCounter(counter+1),[counter]
 
   return (
     <div className="App">
-      <h1>Hello from Parent: {counter}</h1>
-      <button onClick={() => setCounter(counter + 1)} >+1 </button>
-      <input type="text" onChange={e => setInput(e.target.value)} value={input} />
+      <h1>Parent: {counter}</h1>
+      <button onClick={() => setCounter(counter + 1)} >+1 </button><br /><br />
+      <p>Type Some Inputs</p>
+      <input type="text" onChange={e => setInput(e.target.value)} value={input} placeholder="type Input Here !"/>
       <Child counter = {counter} updateCounter={updateCounterFromChild}/>
     </div>
   );
--- src/Child.js
+++ src/Child.js
@@ -1,17 +1,30 @@
 import React from 'react' ;
 import "./Child.css";
 import { memo } from 'react';
+import { useMemo } from 'react';
 
 const Child = props => {
   
     const {counter, updateCounter} = props;
 
 
+      const childNumber = useMemo(() =>{
+        let output = 0;
+      for(let i = 0; i < 500000; i++){
+        output++;
+      }  
+        return output;
+      }, []);
+      // let childNumber = 0;
+      // for(let i = 0; i < 500000; i++){
+      //   childNumber++
+      // }
+
     console.log("Child is Rendering !!! ");
 
     return (
         <div className='Child'> 
-        <h1>Child - {counter}</h1>
+        <h1>Child - {childNumber} {counter}</h1>
         <button onClick={updateCounter}>click</button>
         </div>
   )
